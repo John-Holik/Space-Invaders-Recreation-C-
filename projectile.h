f8e0f709407d9fafde584cbfea8aed5f02d738e6
John-Holik <johnholikiii@outlook.com>
2024-09-24 12:08:52 -0400

    Adding Projectile Header File

--- projectile.h
+++ projectile.h
@@ -1,5 +1,22 @@
 #ifndef PROJECTILE.H
 #define PROJECTILE.H
 
+#include <SFML/Graphics.hpp>
 
-#endif
+class Projectile{
+public:
+    Projectile(float x, float y);
+
+    void update();
+
+    void draw(sf::RenderWindow& window);
+
+    void collision();
+
+private:
+    sf::RectangleShape shape;
+    float speed;
+};
+
+
+#endif // PROJECTILE.H
